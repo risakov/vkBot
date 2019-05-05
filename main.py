99ae8c55f41be686d1758550011a648f547072e9
prevision <romanikani@gmail.com>
2019-05-05 23:40:12 +0300

    New posts checking

--- main.py
+++ main.py
@@ -7,61 +7,82 @@ import sys
 import vk_parsing
 import dateutil.parser as dparser
 
-myUrl = "https://new.broadcast.vkforms.ru/api/v1/user-list/663462?token=159597286_6ef46a6973002ef5c37a58850f1acbe89e3077e2cccf35ca61b6ea1a48cc252d_122434565A5748831A159597286"
-token = "1030fbd1aa04badd72660e75dcddf9212386711adf8c721a0fe314890adebddb94331e345bd5c0e2ec2e5"
-vkSession = vk_api.VkApi(token=token)
-sessionApi = vkSession.get_api()
-longpool = VkLongPoll(vkSession)
+def posting():
+    myUrl = "https://new.broadcast.vkforms.ru/api/v1/user-list/663462?token=api_61099_QpGpiIef1uuqHHKhWwH5i4PA"
+    token = "1030fbd1aa04badd72660e75dcddf9212386711adf8c721a0fe314890adebddb94331e345bd5c0e2ec2e5"
+    vkSession = vk_api.VkApi(token=token)
+    sessionApi = vkSession.get_api()
+    longPool = VkLongPoll(vkSession)
+    urlOfImage = ""
+    owner_id = -159597286
+    dateMain = 0;
+    dateParse = "Tue Apr 30 17:31:53 2019"
+    while True:
+        # get IDs of subscribers in a list
+        try:
+            req = request.Request(myUrl)
+            otvet = request.urlopen(req)
+            otvet = otvet.readlines()
+            listOfIds = []
+            for line in otvet:
+                listOfIds.append(int(line))
+
+        except Exception:
+            print("Error occured during web request!")
+            print(sys.exc_info()[1])
+        try:
+            data = vk_parsing.takeLastPost()
+            vk_parsing.file_writer(data)
+            # creating new list with data of the last post
+            parses = vk_parsing.file_reader()
+            print(parses)
+
+            # Format of string
+
+            dateDefault = time.ctime(int(parses["date"]))
+            print(dateDefault)
+            dateParse1 = dparser.parse(dateDefault)
+            dateParse2 = dparser.parse(dateParse)
+            if dateParse1 > dateParse2:
+                vkSession.method('messages.send',
+                                 {'user_id': 122434565,
+                                  "random_id": 0,
+                                  "message": parses["text"],
+                                  "attachment": "photo" + str(owner_id) + "_" + parses["id1"] + ',' +
+                                                "photo" + str(owner_id) + "_" + parses["id2"] + ',' +
+                                                "photo" + str(owner_id) + "_" + parses["id3"]
+                                  })
+                dateParse = dateDefault
+                time.sleep(15)
+                continue
+            else:
+                time.sleep(15)
+                continue
+
+            '''allDates = []
+            allDates.append(dateParse)
+            allDates.append(dateParse1)
+            allDates.sort()
+            trueDate = allDates[len(allDates) - 1]'''
+            # reaprse to unix
+
+            # global reParse = int(trueDate.timestamp())
+            # print(reParse)
+            # print("photo" + str(owner_id) + "_" + parses["id"])
+        except Exception:
+            print('Posts exception.')
+            print(sys.exc_info()[1])
+
+        '''
+        vkSession.method('messages.send',
+                         {'user_id': 122434565,
+                          "random_id": 0,
+                          "message": parses["text"],
+                          "attachment": "photo" + str(owner_id) + "_" + parses["id1"] + ',' +
+                                        "photo" + str(owner_id) + "_" + parses["id2"] + ',' +
+                                        "photo" + str(owner_id) + "_" + parses["id3"]
+                          })
+        time.sleep(60)'''
+posting()
 
 
-while True:
-    #get IDs of subscribers in a list
-    try:
-        req = request.Request(myUrl)
-        otvet = request.urlopen(req)
-        otvet = otvet.readlines()
-        listOfIds = []
-        for line in otvet:
-            listOfIds.append(int(line))
-        for line in listOfIds:
-            print(line)
-
-    except Exception:
-        print("Error occured during web request!")
-        print(sys.exc_info()[1])
-    try:
-        data = vk_parsing.takeLastPost()
-        vk_parsing.file_writer(data)
-
-        #creating new list with data of the last post
-        parses = vk_parsing.file_reader()
-        print(parses)
-        text = parses[0][0]
-        urlOfImage = parses[0][1]
-
-        #Format of string
-        dateDefault = time.ctime(int(parses[0][2]))
-
-        #Parse to datetime
-        dateParse = dparser.parse(dateDefault)
-        dates = []
-        dates.append(dateParse)
-        dates.sort()
-
-        #reaprse to unix
-        reParse = str(int(dates[len(dates)-1].timestamp()))
-        print(reParse)
-    except Exception:
-        print('Posts exception.')
-
-    #proccessing of the event
-    for event in longpool.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            #print("Сообщение пришло в " + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
-            #print("Текст сообщения: ", str(event.text))
-            id = event.user_id
-            text = event.text
-            response = event.text.lower()
-            if event.from_user:
-                if(response == "1"):
-                    vkSession.method('messages.send', {'user_id': id, "random_id": 0,"message": urlOfImage})
--- vk_parsing.py
+++ vk_parsing.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import csv
+import sys
 from datetime import datetime
 
 #function that just take a last post
@@ -14,8 +15,8 @@ def takeLastPost():
                                 'access_token': token,
                                 'v': version,
                                 'owner_id': owner_id,
-                                'count': 1,
-                                'offset': 0
+                                'count': 2,
+                                'offset': 0,
                             }
                             )
     data = response.json()["response"]["items"]
@@ -25,26 +26,75 @@ def takeLastPost():
 def file_writer(data):
     with open('sales4life.csv', 'w') as file:
         a_pen = csv.writer(file)
-        for post in data:
+        #if post not pinned - except
+            #else this post is needed
+        try:
+            if (data[0]['is_pinned']):
+                try:
+                    if data[1]['attachments'][0]['type']:
+                        try:
+                            media_id1 = data[1]['attachments'][0]['photo']['id']
+                        except Exception:
+                            media_id1 = " "
+                        try:
+                            media_id2 = data[1]['attachments'][1]['photo']['id']
+                        except Exception:
+                            media_id2 = " "
+                        try:
+                            media_id3 = data[1]['attachments'][2]['photo']['id']
+                        except Exception:
+                            media_id3 = " "
+                    a_pen.writerow((data[1]['text'], media_id1, media_id2, media_id3, data[1]['date']))
+                except Exception:
+                    a_pen.writerow((data[1]['text'], " ", " ", " ", data[1]['date']))
+                    pass
+        except Exception:
             try:
-                if post['attachments'][0]['type']:
-                    img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
-                else:
-                    img_url = 'pass'
+                if data[0]['attachments'][0]['type']:
+                    try:
+                        media_id1 = data[0]['attachments'][0]['photo']['id']
+                    except Exception:
+                        media_id1 = " "
+                    try:
+                        media_id2 = data[0]['attachments'][1]['photo']['id']
+                    except Exception:
+                        media_id2 = " "
+                    try:
+                        media_id3 = data[0]['attachments'][2]['photo']['id']
+                    except Exception:
+                        media_id3 = " "
+                    a_pen.writerow((data[0]['text'], media_id1, media_id2, media_id3, data[0]['date']))
             except Exception:
+                a_pen.writerow((data[0]['text'], " ", " ", " ", data[0]['date']))
                 pass
-            a_pen.writerow((post['text'], img_url, post['date']))
 
-#function that read created file and write data in list
+
+
+
+#function that read a created file and write data in the list
 def file_reader():
     with open("sales4life.csv", 'r') as file:
         a_pen = csv.reader(file)
         listOfData = []
         for row in a_pen:
             listOfData.append(row)
-        return listOfData
+        dictOfData = {}
+        dictOfData["text"] = listOfData[0][0]
+        dictOfData["id1"] = listOfData[0][1]
+        dictOfData["id2"] = listOfData[0][2]
+        dictOfData["id3"] = listOfData[0][3]
+        dictOfData["date"] = listOfData[0][4]
 
-'''token = "8e8d74f28e8d74f28e8d74f2618ee7358388e8d8e8d74f2d249bba3b1d4725d1db9525e"
+
+        '''dictOfData = {}
+        dictOfData["text"] = listOfData[0]
+        dictOfData["id"] = listOfData[1]
+        dictOfData["date"] = listOfData[2]
+        dictOfData["is_pinned"] = listOfData[3]'''
+        return dictOfData
+
+'''
+token = "8e8d74f28e8d74f28e8d74f2618ee7358388e8d8e8d74f2d249bba3b1d4725d1db9525e"
 owner_id = '-159597286'
 version = 5.95
 response = requests.get('https://api.vk.com/method/wall.get',
@@ -52,9 +102,8 @@ response = requests.get('https://api.vk.com/method/wall.get',
                             'access_token': token,
                             'v': version,
                             'owner_id': owner_id,
-                            'count': 1,
+                            'count': 2,
                             'offset': 0
                             }
                             )
-data = response.json()
-print(1)'''
+data = response.json()'''
